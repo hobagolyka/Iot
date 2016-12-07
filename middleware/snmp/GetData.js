1661ecc455dd82eef9ba28d0a033136bbf8246f3
hobagolyka <toth.jdt@gmail.com>
2016-12-07 10:53:41 +0100

    update

--- middleware/snmp/GetData.js
+++ middleware/snmp/GetData.js
@@ -7,64 +7,69 @@ module.exports = function () {
     return function (req, res, next) {
         var io = res.tpl.socket;
         var IP = res.tpl.adatok.IP;
+        res.tpl.d = 'hello';
 
-            var session = snmp.createSession (IP, "public");
+        var session = snmp.createSession (IP, "public");
 
-            var oid = "1.3.6.1.2.1.2.2";
-            var columns = [2, 6];
+        var oid = "1.3.6.1.2.1.2.2";
+        var columns = [2];
 
-            function sortInt (a, b) {
-                if (a > b)
-                    return 1;
-                else if (b > a)
-                    return -1;
-                else
-                    return 0;
-            }
+        function sortInt (a, b) {
+            if (a > b)
+                return 1;
+            else if (b > a)
+                return -1;
+            else
+                return 0;
+        }
 
-            function responseCb (error, table) {
-                if (error) {
-                    console.error (error.toString ());
-                } else {
-                    // This code is purely used to print rows out in index order,
-                    // ifIndex's are integers so we'll sort them numerically using
-                    // the sortInt() function above
-                    var indexes = [];
-                    for (index in table)
-                        indexes.push (parseInt (index));
-                    indexes.sort (sortInt);
+        function responseCb (error, table) {
+            res.tpl.t = JSON.stringify(table);
+            res.tpl.sorti = sortInt;
 
-                    // Use the sorted indexes we've calculated to walk through each
-                    // row in order
-                    for (var i = 0; i < indexes.length; i++) {
-                        // Like indexes we sort by column, so use the same trick here,
-                        // some rows may not have the same columns as other rows, so
-                        // we calculate this per row
-                        var columns = [];
-                        for (column in table[indexes[i]])
-                            columns.push (parseInt (column));
-                        columns.sort (sortInt);
+            if (error) {
+                console.error (error.toString ());
+            } else {
+                // This code is purely used to print rows out in index order,
+                // ifIndex's are integers so we'll sort them numerically using
+                // the sortInt() function above
+                var indexes = [];
+                for (index in table)
+                    indexes.push (parseInt (index));
+                indexes.sort (sortInt);
 
-                        // Print index, then each column indented under the index
-                        console.log ("row for index = " + indexes[i]);
-                        for (var j = 0; j < columns.length; j++) {
-                            console.log ("   column " + columns[j] + " = "
-                                + table[indexes[i]][columns[j]]);
-                        }
+                // Use the sorted indexes we've calculated to walk through each
+                // row in order
+                for (var i = 0; i < indexes.length; i++) {
+                    // Like indexes we sort by column, so use the same trick here,
+                    // some rows may not have the same columns as other rows, so
+                    // we calculate this per row
+                    var columns = [];
+                    for (column in table[indexes[i]])
+                        columns.push (parseInt (column));
+                    columns.sort (sortInt);
+
+                    // Print index, then each column indented under the index
+                    console.log ("row for index = " + indexes[i]);
+                    for (var j = 0; j < columns.length; j++) {
+                        console.log ("   column " + columns[j] + " = "
+                            + table[indexes[i]][columns[j]]);
                     }
                 }
             }
+            return next();
+        }
 
-            var maxRepetitions = 20;
+        var maxRepetitions = 20;
 
 // The maxRepetitions argument is optional, and will be ignored unless using
 // SNMP verison 2c
-            session.tableColumns (oid, columns, maxRepetitions, responseCb);
+        session.tableColumns (oid, columns, maxRepetitions, responseCb);
             session.trap (snmp.TrapType.LinkDown, function (error) {
                 if (error)
                     console.error (error);
             });
-        return next();
+
     };
 
 };
--- middleware/snmp/runSnmp.js
+++ middleware/snmp/runSnmp.js
@@ -11,7 +11,14 @@ module.exports = function () {
 
             var session = snmp.createSession(IP, "public");
 
-            var basic_oids = ["1.3.6.1.2.1.1.1.0", "1.3.6.1.2.1.1.2.0", "1.3.6.1.2.1.1.3.0", "1.3.6.1.2.1.1.4.0", "1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0"];
+            var basic_oids = [
+                "1.3.6.1.2.1.1.1.0",	 /* sysDescr */
+                "1.3.6.1.2.1.1.2.0",	 /* sysObjectID */
+                "1.3.6.1.2.1.1.3.0",	 /* sysUpTime */
+                "1.3.6.1.2.1.1.4.0",	 /* sysContact */
+                "1.3.6.1.2.1.1.5.0",	 /* sysName */
+                "1.3.6.1.2.1.1.6.0"	   	 /* sysLocation */
+            ];
 
             session.get(basic_oids, function (error, varbinds) {
                 if (error) {
